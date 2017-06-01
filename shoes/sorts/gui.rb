75b824b792e5e978800286acaf0bbb4daeb023ee
Jake Helman <jacob.helman@hbdi.com>
2017-06-01 08:18:01 -0400

    getting shoes gui ready to animate some sorts

--- shoes/sorts/gui.rb
+++ shoes/sorts/gui.rb
@@ -1,26 +1,52 @@
 # require 'insertion.rb'
 
 def generate_random_array
-  (0..100).to_a.shuffle
+  (1..100).to_a.shuffle
 end
 
-Shoes.app(width: 400, height: 400) do
-  background "#DFA"
-  para "Sort Visualizer"
+def sort_array(arr)
+  arr.sort
+end
 
-  @array = generate_random_array
-
-  # draw graph
-  stack(top: 50, left: 50, width: 300, height: 100) do
-    border black, strokewidth: 2
-    fill red
-    @array.each_with_index do |element,i|
-      rect(left: 3*i,
-        bottom: 100,
-        top: 100 - element,
-        width: 2)
+Shoes.app(width: 400, height: 500) do
+  def update_chart()
+    @chart.clear
+    @chart.append do
+      border black, strokewidth: 2
+      fill red
+      @array.each_with_index do |element,i|
+        rect(left: 4*i,
+          bottom: 400,
+          top: (100 - element) * 4,
+          width: 4)
+      end
     end
   end
 
-
+  flow width: 400, height: 500 do
+    background "#DFA"
+    # Title bar
+    flow width: 1.0, height: 0.1 do
+      caption "Sort Visualizer"
+    end
+    @array = generate_random_array
+    # Chart window
+    @chart = flow width: 1.0, height: 0.8 do
+      border black, strokewidth: 2
+      fill red
+      @array.each_with_index do |element,i|
+        rect(left: 4*i,
+          bottom: 400,
+          top: (100 - element) * 4,
+          width: 4)
+      end
+    end
+    # Sort button panel
+    flow width: 1.0, height: 0.1 do
+      button 'Insertion', width: 1.0, height: 1.0 do
+        @array = sort_array(@array)
+        update_chart
+      end
+    end
+  end
 end
